87b23d17892f542fde48036133062f3b3cb775c1
João Oliveira <hello@jxs.pt>
2019-08-27 22:56:46 +0100

    use html scrapping instead of json api for query search

--- src/letterboxd.rs
+++ src/letterboxd.rs
@@ -1,124 +1,60 @@
-use std::time::{SystemTime, UNIX_EPOCH};
-
-use failure::{format_err, Error, ResultExt};
+use failure::{format_err, Error};
 use futures::TryStreamExt;
-use hmac::{Hmac, Mac};
 use hyper::Client;
 use hyper_tls::HttpsConnector;
-use serde::Deserialize;
-use sha2::Sha256;
+use select::document::Document;
+use select::node::Node;
+use select::predicate::{Class, Name, Predicate};
 use telegram_bot::{InlineQueryResult, InlineQueryResultArticle, InputTextMessageContent};
-use uuid::Uuid;
 
-#[derive(Deserialize, Debug)]
-pub struct LbResults {
-    items: Vec<FilmSearch>,
-}
+const LETTERBOXD_URL: &str = "https://letterboxd.com";
 
-#[derive(Deserialize, Debug, Default)]
-pub struct FilmSearch {
-    film: Option<Film>,
-}
-
-#[derive(Deserialize, Debug, Default)]
-pub struct Film {
-    id: String,
-    name: String,
-    poster: Option<Posters>,
-    links: Vec<Link>,
-}
-
-#[derive(Deserialize, Debug, Default)]
-pub struct Posters {
-    sizes: Vec<Link>,
-}
-
-#[derive(Deserialize, Debug, Default)]
-pub struct Link {
-    url: String,
-}
-
-pub struct Api {
-    key: String,
-    secret: String,
-}
-
-impl Api {
-    pub fn new(key: &str, secret: &str) -> Api {
-        Api {
-            key: key.to_string(),
-            secret: secret.to_string(),
-        }
-    }
-
-    pub async fn search(&self, query: &str) -> Result<Vec<InlineQueryResult>, Error> {
-        letterboxd_search(self.key.clone(), self.secret.clone(), query.to_string()).await
-    }
-}
-
-pub async fn letterboxd_search(
-    key: String,
-    secret: String,
-    query: String,
-) -> Result<Vec<InlineQueryResult>, Error> {
+pub async fn search(query: &str) -> Result<Vec<InlineQueryResult>, Error> {
     let https = HttpsConnector::new(1)?;
     let client = Client::builder().build::<_, hyper::Body>(https);
 
-    let nonce = Uuid::new_v4().to_simple();
-    let timestamp = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .context(format!("System Time is invalid"))?
-        .as_secs();
-
-    let url = format!(
-        "https://api.letterboxd.com/api/v0/search?apikey={}&nonce={}&timestamp={}&input={}&perPage=5",
-        key,
-        nonce,
-        timestamp,
-        urlencoding::encode(&query)
-    );
-    let salted = format!("GET\u{0000}{}\u{0000}", url);
-    let mut mac = Hmac::<Sha256>::new_varkey(secret.as_bytes())
-        .map_err(|err| format_err!("invalid sha256 key, {}", err))?;
-    mac.input(salted.as_bytes());
-    let result = hex::encode(&mac.result().code().to_vec()).to_lowercase();
-
-    let url = format!("{}&signature={}", url, result)
+    let url = format!("{}/search/{}/", LETTERBOXD_URL, urlencoding::encode(&query))
         .parse::<hyper::Uri>()
         .unwrap();
-
-    let res = client.get(url)
-        .await?;
+    let res = client.get(url).await?;
 
     let body = res.into_body().try_concat().await?;
 
-    let response: LbResults = serde_json::from_slice(body.as_ref())?;
+    let document = Document::from_read(body.as_ref())?;
 
-    let results = response
-        .items
-        .into_iter()
-        .filter_map(|film_search| film_search.film)
-        .map(film_to_article)
-        .map(From::from)
-        .collect();
-
-    Ok(results)
+    document
+        .find(Class("results").descendant(Name("div")))
+        .filter(|node| node.attr("data-film-name").is_some())
+        .take(10)
+        .map(node_to_article)
+        .collect()
 }
 
-pub fn film_to_article(mut film: Film) -> InlineQueryResultArticle {
-    let mut message = InputTextMessageContent {
-        message_text: "".into(),
+pub fn node_to_article(node: Node) -> Result<InlineQueryResult, Error> {
+    let target_link = get_node_attr(&node, "data-target-link")?;
+    let film_id = get_node_attr(&node, "data-film-id")?;
+    let name = get_node_attr(&node, "data-film-name")?;
+    let year = get_node_attr(&node, "data-film-release-year")?;
+
+    let message = InputTextMessageContent {
+        message_text: format!("{}{}", LETTERBOXD_URL, target_link),
         parse_mode: Some(telegram_bot::ParseMode::Html),
         disable_web_page_preview: false,
     };
 
-    if film.links.len() > 0 {
-        message.message_text = film.links.remove(0).url;
+    let mut article =
+        InlineQueryResultArticle::new(film_id, format!("{} ({})", name, year), message);
+    let posters: Vec<&str> = node
+        .find(Name("img"))
+        .filter_map(|node| node.attr("src"))
+        .collect();
+    if !posters.is_empty() {
+        article.thumb_url(posters[0]);
     }
+    Ok(From::from(article))
+}
 
-    let mut article = InlineQueryResultArticle::new(film.id, film.name, message);
-    if film.poster.is_some() {
-        article.thumb_url(film.poster.unwrap().sizes.remove(0).url);
-    }
-    article
+fn get_node_attr<'a>(node: &'a Node, attr: &str) -> Result<&'a str, Error> {
+    node.attr(attr)
+        .ok_or_else(|| format_err!("missing {}", attr))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,21 @@
-
 mod letterboxd;
 
 use std::time::Duration;
 
 use futures::StreamExt;
-use tokio::timer::Timeout;
-use letterboxd::Api as LetterboxdApi;
 use telegram_bot::{
-    Api as TelegramBotApi, CanAnswerInlineQuery,
-    CanReplySendMessage, InlineQueryResult, Update, UpdateKind,
+    Api as TelegramBotApi, CanAnswerInlineQuery, CanReplySendMessage, InlineQuery,
+    InlineQueryResult, Update, UpdateKind,
 };
+use tokio::timer::Timeout;
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
     let telegram_token =
         std::env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not found in environment");
-    let letterboxd_key =
-        std::env::var("LETTERBOXD_API_KEY").expect("LETTERBOXD_API_KEY not found in environment");
-    let letterboxd_secret = std::env::var("LETTERBOXD_API_SECRET")
-        .expect("LETTERBOXD_API_SECRET not found in environment");
     let telegram_api = TelegramBotApi::new(&telegram_token);
 
-    let letterboxd_api = LetterboxdApi::new(&letterboxd_key, &letterboxd_secret);
     let mut stream = telegram_api.stream();
 
     log::info!("starting good reads telegram bot!");
@@ -31,28 +24,9 @@ async fn main() {
             Ok(Update {
                 kind: UpdateKind::InlineQuery(query),
                 ..
-            }) => match Timeout::new(letterboxd_api.search(&query.query), Duration::new(5, 0)).await {
-                Ok(Ok(results)) => {
-                    let reply = query.answer(results);
-                    let res = telegram_api.send(reply).await;
-                    if let Err(err) = res {
-                        log::error!("telegram bot send error, {:?}", err);
-
-                    }
-                }
-                Err(_elapsed) => {
-                    log::error!("request to letterboxd Api timedout after 5 seconds");
-                    let empty: Vec<InlineQueryResult> = vec![];
-                    let reply = query.answer(empty);
-                    let res = telegram_api.send(reply).await;
-                    if let Err(err) = res {
-                        log::error!("telegram bot send error, {:?}", err);
-                    }
-                }
-                Ok(Err(err)) => {
-                    log::error!("update error, {}", err);
-                }
-            },
+            }) => {
+                tokio::spawn(get_query_send_response(telegram_api.clone(), query));
+            }
             Ok(Update {
                 kind: UpdateKind::Message(message),
                 ..
@@ -70,6 +44,30 @@ async fn main() {
                 log::error!("update error, {}", err);
             }
             _ => {}
+        }
+    }
+}
+
+async fn get_query_send_response(telegram_api: TelegramBotApi, query: InlineQuery) {
+    match Timeout::new(letterboxd::search(&query.query), Duration::new(5, 0)).await {
+        Ok(Ok(results)) => {
+            let reply = query.answer(results);
+            let res = telegram_api.send(reply).await;
+            if let Err(err) = res {
+                log::error!("telegram bot send error, {:?}", err);
+            }
+        }
+        Err(_elapsed) => {
+            log::error!("request to letterboxd Api timedout after 5 seconds");
+            let empty: Vec<InlineQueryResult> = vec![];
+            let reply = query.answer(empty);
+            let res = telegram_api.send(reply).await;
+            if let Err(err) = res {
+                log::error!("telegram bot send error, {:?}", err);
+            }
+        }
+        Ok(Err(err)) => {
+            log::error!("search query error, {}", err);
         }
     }
 }
